b2b0344272edf4cca1bdc011afa89b3cb2a43d17
Nikhil <97503662+nikhilhvr@users.noreply.github.com>
2022-06-23 19:00:02 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import './components/navbar.css';
+import './components/Home.css';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+
 
 function App() {
   return(
     <>
-    <Navbar title ="EduDron" aboutText= "About US" />
+    <Navbar/>
+    <Home/>
+    
     </>
   );
 }
