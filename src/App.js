b25a004fb4c3887b988782db553f1d60af9ff012
Abhishek-Kumar <abhishekphd14@gmail.com>
2022-06-23 19:07:52 +0530

    TextForm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return(
     <>
-    <Navbar title ="EduDron" aboutText= "About US" />
+    
+     
+    
     </>
   );
 }
