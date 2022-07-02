c90112ef36d560efa4f8b14ee2196456d97b765a
Nikhil <97503662+nikhilhvr@users.noreply.github.com>
2022-07-02 11:51:41 +0530

    sub-nav

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import './PageCss/footer.css';
 import Navbar from './components/Navbar';
 import Home from './Home';
 import Footer from './components/Footer';
+// import AboutUs from './components/AboutUs';
+import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 // import Title from './Title'
 // import TextForm from './components/TextForm';
 
@@ -13,14 +16,18 @@ function App() {
   return(
     
     
-     <> 
- 
-    <Navbar/>
-    {/* <Title/> */}
-    <Home/>
-
-    <Footer/>
-    
+     <>
+     {/* <BrowserRouter> */}
+    {/* <Routes> */}
+      <Navbar/>
+      <Home/>
+      {/* <Route path="/" element={<Home/>}> */}
+        {/* <Route path="/aboutus" element={<AboutUs/>}> */}
+        {/* </Route> */}
+      {/* </Route> */}
+      <Footer/>
+    {/* </Routes> */}
+  {/* </BrowserRouter> */}
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { propTypes } from 'react-bootstrap/esm/Image'
+import { Link } from "react-router-dom";
+// import { propTypes } from 'react-bootstrap/esm/Image'
 
 export default function Navbar(props) {
   return (
@@ -9,25 +10,66 @@ export default function Navbar(props) {
     <input id="nav-toggle" type="checkbox"/>
     <div class="logo">Edu<strong>Dron</strong></div>
     <ul class="links">
-      <li><a href="#home">Home</a></li>
-      <li><a href="#about">About</a>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/aboutus">About</Link></li>
+
+      <li><a href="#about">Colleges</a>
+        <div class='sub-nav'>
+        <ul>
+        <li className='Hover-sub-nav'> <a href="#">Stream</a>
+        <div className='sub-nav-2'>
+          <ul>
+            <li><a href="">Management</a></li>
+            <li><a href="">Engineering</a></li>
+            <li><a href="">Pharmacy</a></li>
+            <li><a href="">Law</a></li>
+            <li><a href="">More</a></li>
+          </ul>
+        </div>
+        </li>
+        <li className='Hover-sub-nav'><a href="#">State</a>
+        <div className='sub-nav-2'>
+          <ul>
+            <li><Link to="">North State</Link></li>
+            <li><Link to="">South State</Link></li>
+            <li><Link to="">North East State</Link></li>
+            <li><Link to="">North West State</Link></li>
+          </ul>
+        </div></li>
+
+        <li className='Hover-sub-nav'><a href="#">Courses</a>
+        <div className='sub-nav-2'>
+          <ul>
+            <li><a href="">Programming</a></li>
+            <li><a href="">Maching learning</a></li>
+            <li><a href="">Data Science</a></li>
+            <li><a href="">Cyber Security</a></li>
+            <li><a href=""></a></li>
+          </ul>
+        </div></li>
+      </ul>
+      </div></li>
+
+      <li><a href="#Services">Services</a>
       <div class='sub-nav'>
         <ul>
-        <li><a href="#">Nikhil</a></li>
-        <li><a href="#">Nikhil</a></li>
-        <li><a href="#">Nikhil</a></li>
-        <li><a href="#">Nikhil</a></li>
+        <li><a href="#">Our Courses</a></li>
+        <li><a href="#">counselling</a></li>
+        <li><a href="#">Mentorship</a></li>
+        <li><a href="#">Training</a></li>
+        <li><a href="#">Intership</a></li>
+        <li><a href="#">Study Abroad</a></li>
+        <li><a href="#">Career</a></li>
+        <li><a href="#">More...</a></li>
         </ul>
-      </div>
-      </li>
-      <li><a href="#Services">Services</a></li>
+      </div></li>
       <li><a href="#Contact Us">Contact Us</a></li>
       <button id="login" href="#Login">Login</button>
       <button id='sign-up' href="#Login">Sign Up</button>
     </ul>
     
     
-    <label for="nav-toggle" class="icon-burger">
+    <label for="nav-toggle" className="icon-burger">
       <div class="line"></div>
       <div class="line"></div>
       <div class="line"></div>
