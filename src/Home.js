287bedc8ad80e61bc905a825c10afad215feff42
Abhishek-Kumar <abhishekphd14@gmail.com>
2022-07-06 15:18:06 +0530

    react-dom bug

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,22 @@
-import React from 'react'
-import './PageCss/Home.css'
-import './PageCss/subscription.css'
-import UpperTitle from './homepage/UpperTitle'
-import Subscription from './components/Subscription'
-
-
+import React from "react";
+import "./PageCss/Home.css";
+import "./PageCss/subscription.css";
+import UpperTitle from "./homepage/UpperTitle";
+import Subscription from "./components/Subscription";
 
 function Home() {
   return (
     <>
-    <div className="Home">
-      <UpperTitle/>
-      
-    </div>
-    
-    
-    <div className='abhi'> 
-        
-    </div>
-    <div><Subscription/></div>
-    
+      <div className="Home">
+        <UpperTitle />
+      </div>
+
+      <div className="abhi"></div>
+      <div>
+        <Subscription />
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
