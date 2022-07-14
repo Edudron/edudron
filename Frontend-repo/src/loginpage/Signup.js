0953d713c0a0dcf9dda86841f739eaceb3da6f00
Abhishek-Kumar <abhishekphd14@gmail.com>
2022-07-14 21:40:48 +0530

    setting up backend firebase, Database and mongodb

--- Frontend-repo/src/loginpage/Signup.js
+++ Frontend-repo/src/loginpage/Signup.js
@@ -1,69 +1,109 @@
-import React, {useState,setState} from 'react';
+import React, { useState, setState } from "react";
 import "../PageCss/Signup.css";
 
 function SignupForm() {
-    
-    const [firstName, setFirstName] = useState(null);
-    const [lastName, setLastName] = useState(null);
-    const [email, setEmail] = useState(null);
-    const [password,setPassword] = useState(null);
-    const [confirmPassword,setConfirmPassword] = useState(null);
+  const [username, setFirstName] = useState(null);
+  const [fullname, setfullname] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState(null);
 
-    const handleInputChange = (e) => {
-        const {id , value} = e.target;
-        if(id === "firstName"){
-            setFirstName(value);
-        }
-        if(id === "lastName"){
-            setLastName(value);
-        }
-        if(id === "email"){
-            setEmail(value);
-        }
-        if(id === "password"){
-            setPassword(value);
-        }
-        if(id === "confirmPassword"){
-            setConfirmPassword(value);
-        }
-
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    if (id === "username") {
+      setFirstName(value);
     }
-
-    const handleSubmit  = () => {
-        console.log(firstName,lastName,email,password,confirmPassword);
+    if (id === "fullname") {
+      setfullname(value);
     }
+    if (id === "email") {
+      setEmail(value);
+    }
+    if (id === "password") {
+      setPassword(value);
+    }
+    if (id === "confirmPassword") {
+      setConfirmPassword(value);
+    }
+  };
 
-    return(
-        <div className="sign-form">
-            <div className="form-title">SignUp
-            <div className="form-body">
-                <div className="username">
-                    <label className="form__label" for="firstName">First Name: </label>
-                    <input className="form__input" type="text" value={firstName} onChange = {(e) => handleInputChange(e)} id="firstName" />
-                </div>
-                <div className="lastname">
-                    <label className="form__label" for="lastName">Last Name: </label>
-                    <input  type="text" name="" id="lastName" value={lastName}  className="form__input" onChange = {(e) => handleInputChange(e)} />
-                </div>
-                <div className="email">
-                    <label className="form__label" for="email">Email: </label>
-                    <input  type="email" id="email" className="form__input" value={email} onChange = {(e) => handleInputChange(e)} />
-                </div>
-                <div className="password">
-                    <label className="form__label" for="password">Password: </label>
-                    <input className="form__input" type="password"  id="password" value={password} onChange = {(e) => handleInputChange(e)} />
-                </div>
-                <div className="confirm-password">
-                    <label className="form__label" for="confirmPassword">Confirm Password: </label>
-                    <input className="form__input" type="password" id="confirmPassword" value={confirmPassword} onChange = {(e) => handleInputChange(e)} />
-                </div>
-            </div>
-            <div class="end">
+  const handleSubmit = () => {
+    console.log(username, fullname, email, password, confirmPassword);
+  };
+
+  return (
+    <div className="sign-form">
+      <div className="form-title">
+        SignUp
+        <div className="form-body">
+          <div className="username">
+            <label className="form__label" for="username">
+              Username:{" "}
+            </label>
+            <input
+              className="form__input"
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+          <div className="fullname">
+            <label className="form__label" for="fullname">
+              Full Name:{" "}
+            </label>
+            <input
+              type="text"
+              name=""
+              id="fullname"
+              value={fullname}
+              className="form__input"
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+          <div className="email">
+            <label className="form__label" for="email">
+              Email:{" "}
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="form__input"
+              value={email}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+          <div className="password">
+            <label className="form__label" for="password">
+              Password:{" "}
+            </label>
+            <input
+              className="form__input"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+          <div className="confirm-password">
+            <label className="form__label" for="confirmPassword">
+              Confirm Password:{" "}
+            </label>
+            <input
+              className="form__input"
+              type="password"
+              id="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+        </div>
+        <div class="end">
                 <input type="submit" value="Register" name=""></input>
             </div>
-        </div>
-        </div>
-       
-    );      
+      </div>
+    </div>
+     
+  );
 }
-export default SignupForm ;
+export default SignupForm;
