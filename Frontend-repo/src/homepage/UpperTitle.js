f78d1a6ee4738d3dca72a5e08c95e8b4399d2171
Abhishek-Kumar <abhishekphd14@gmail.com>
2022-07-21 16:58:19 +0530

    testing

--- Frontend-repo/src/homepage/UpperTitle.js
+++ Frontend-repo/src/homepage/UpperTitle.js
@@ -6,7 +6,7 @@ function UpperTitle() {
   return (
     <>
     <div className="UpperTitle">
-      <h1>Welcome To EduDron Community</h1>
+      <h1>Welcome To EduDron Communityyyy</h1>
     </div>
     <div className="container-upper">
         </div>
